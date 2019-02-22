843257682953c1854af216be5e889a178fbff71f
Scott Boag <sboag@Scotts-MBP-2.home>
2019-02-22 17:58:03 -0500

    For the moment, comment out call to extendLearnerContainer

--- service/lcm/container_helper.go
+++ service/lcm/container_helper.go
@@ -479,7 +479,7 @@ func constructLearnerContainer(req *service.JobDeploymentRequest, envVars []v1co
 	}
 
 	learnerContainer := learner.CreateContainerSpec(container, req.Labels["kube_major"], req.Labels["kube_minor"])
-	extendLearnerContainer(&learnerContainer, req, logr)
+	// extendLearnerContainer(&learnerContainer, req, logr)
 	return learnerContainer
 }
 
